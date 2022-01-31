2d72ae2d54261b406e1c246b44342e653ce69e76
Alexandre Eugene <you@example.com>
2022-01-31 22:33:15 +0100

    correction padding contact

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -37,7 +37,7 @@ const titleRef = useRef()
 
 
     const { theme, toggleTheme } = useContext(ThemeContext)
-    console.log(theme);
+    
     
     return (
         <header className='header-container'>
